7bd29641a74f27827252fc8d96deffa197fb34e4
CristianJhair <cristianjhair2000@gmail.com>
2023-11-14 12:11:16 -0500

    db and readme updates

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # php-crud
 Create, delete, edit and update tasks and descriptions.
 
-## Captura de app
+## Steps
+1. Clone this repo within `C:/xampp/htdocs` path.
+2. Open Xampp and start Apache and Mysql services.
+3. Go to `localhost/phpmyadmin`, create a new database called `php_mysql_crud` and import the query `php_mysql_crud.sql` from `database` folder.
+4. Finally, go to `localhost/php-crud` to test the CRUD app.
+
+## Screenshot
 <img width="869" alt="screenshot" src="https://github.com/CristianJhair/php-crud/assets/44480959/8274b39a-f2e7-4ec7-83ea-1e699a319763">
---
+++ database/php_mysql_crud.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 14, 2023 at 05:53 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `php_mysql_crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task`
+--
+
+CREATE TABLE `task` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `task`
+--
+
+INSERT INTO `task` (`id`, `title`, `description`, `created_at`) VALUES
+(3, 'sleep', 'in the bed', '2023-10-26 06:05:26'),
+(4, 'caminar', 'en el parque', '2023-10-26 14:02:45'),
+(18, 'correr', 'salir a correr por la mañana', '2023-11-11 04:16:06');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `task`
+--
+ALTER TABLE `task`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `task`
+--
+ALTER TABLE `task`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
